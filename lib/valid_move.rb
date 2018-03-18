b9e05face2f957f225e7f2a9acc9b6da55ded127
David Ritchey <ritchey0713@gmail.com>
2018-03-18 02:21:12 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,3 +1,18 @@
+def position_taken?(array, index)
+if
+  array[index]== "" || array[index]== " " || array[index] == nil
+  return false
+else
+  true
+end
+
+
+
+
+
 def valid_move?(board, index)
-  
+  if position_taken == false
+    return true
+  else
+    return false
 end
